b3d2c3c7491c5f375fa8ccccde6049739136deb6
ADUDLEY <ashtondudley03@gmail.com>
2025-11-19 01:04:33 -0400

    added tests

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -9,7 +9,17 @@ int main() {
     Vec2 B{1.0f, 2.0f};
 
     Vec2 C = A + B;
+    Vec2 D = A - B;
+    Vec2 E = 2 * A;
+    Vec2 F = A * 2;
 
-    std::cout << C << std::endl;
+    std::cout << "A: " << A << std::endl;
+    std::cout << "B: " << B << std::endl;
 
+    std::cout << "A + B: " << C << std::endl;
+    std::cout << "A - B: " << D << std::endl;
+    std::cout << "2 * A: " << E << std::endl;
+    std::cout << "A * 2: " << F << std::endl;
+
+    return 0;
 }
