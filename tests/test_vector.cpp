6f89cea9412b39868cadbe628da83a5584230cf9
ADUDLEY <ashtondudley03@gmail.com>
2025-11-19 00:35:03 -0400

    working vector test

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,9 +1,14 @@
-#include<iostream>
-
+#include <iostream>
+#include "Math.hpp"
 
 
 int main() {
     std::cout << "Starting Vector Test" << std::endl;
+    
+    Vec2 A{1.0f, 2.0f};
+    Vec2 B{1.0f, 2.0f};
+
+    Vec2 C = A + B;
 
 
 }
